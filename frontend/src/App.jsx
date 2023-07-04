e763c4306aacd4b30fe518911e70fe338c1a0cb3
Joselias <joselias.dev@gmail.com>
2023-07-04 12:33:16 -0300

    feat header and background app color

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,19 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Landing from './components/Landing/Landing'
 import Header from './components/Header/Header'
+import { ThemeProvider } from '@mui/material/styles'
+import theme from './config/temeConfig'
+import './App.css'
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path='/' element={<Landing />} />
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Landing />} />
+        </Routes>
+      </ThemeProvider>
     </BrowserRouter>
   )
 }
