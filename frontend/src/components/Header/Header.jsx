81d02fc1af7c7c334f1f00ccfe81aa7a1196720a
Jose Elias Morales <joselias.dev@gmail.com>
2023-07-04 12:42:20 -0300

    feat header and background app color (#10)

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,8 +1,56 @@
-import React from 'react'
+import Container from '@mui/material/Container'
+import AppBar from '@mui/material/AppBar'
+import logo from '../../assets/logo.png'
+import Box from '@mui/material/Box'
+import IconButton from '@mui/material/IconButton'
+import MenuIcon from '@mui/icons-material/Menu'
+import { useState } from 'react'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import Typography from '@mui/material/Typography'
+import { Link } from 'react-router-dom'
+import Toolbar from '@mui/material/Toolbar'
+import './header.css'
+
+const pages = ['Nosotros', 'Beneficios', 'Ayuda']
 
 const Header = () => {
+  const [anchorNav, setAnchorNav] = useState(null)
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorNav(event.currentTarget)
+  }
+
+  const handleCloseNavMenu = () => {
+    setAnchorNav(null)
+  }
+
   return (
-    <div>Header</div>
+    <AppBar position='static' className='header'>
+      <Container maxWidth='xl'>
+        <Toolbar disableGutters>
+          <img src={logo} alt='logo' width='100px' />
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }} justifyContent='flex-end'>
+            <IconButton size='large' aria-label='account' aria-controls='menu-appbar' aria-haspopup='true' onClick={handleOpenNavMenu} color='inherit'>
+              <MenuIcon />
+            </IconButton>
+            <Menu id='menu-appbar' anchorEl={anchorNav} anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }} open={Boolean(anchorNav)} onClose={handleCloseNavMenu} sx={{ display: { xs: 'block', md: 'none' } }}>
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign='center'><Link>{page}</Link></Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Typography variant='h5' noWrap component='a' sx={{ mr: 2, display: { xs: 'flex', md: 'none' }, flexGrow: 1, color: 'inherit', textDecoration: 'none' }}> Nombre</Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }} justifyContent='space-around'>
+            {pages.map((page) => (
+              <Link key={page} onClick={handleCloseNavMenu} sx={{ my: 2, display: 'block' }}>{page}</Link>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   )
 }
 
---
+++ frontend/src/config/temeConfig.js
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1b2c4a'
+    },
+    text: {
+      primary: '#292929',
+      secondary: '#fdfdfe'
+    }
+  }
+})
+
+export default theme
