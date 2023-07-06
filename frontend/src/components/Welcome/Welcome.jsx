fd7fe69de283d974d52ccdaebbde173031251689
Jose Elias Morales <joselias.dev@gmail.com>
2023-07-06 15:34:53 -0300

    feat: cards home (#24)

--- frontend/src/components/Welcome/Welcome.jsx
+++ frontend/src/components/Welcome/Welcome.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
+import { Typography } from '@mui/material'
+import './welcome.css'
 
 const Welcome = () => {
   return (
-    <section style={{ position: 'relative', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', gap: '1rem', margin: 'auto', marginTop: '150px' }}>
-      <div className='text-wrapper'>Hola Francisco,</div>
-      <h1 className='h-1'>Bienvenido</h1>
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', gap: '1rem' }}>
-        <div className='element' style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-          <span className='span'>$ </span>
-          <span className='text-wrapper-8'>0,00</span>
+    <section style={{ position: 'relative', display: 'flex', flexDirection: 'column', gap: '1rem', marginTop: '150px', margin: 'auto' }}>
+      <div>
+        <Typography variant='h4' color='secondary'>Hola Francisco,</Typography>
+        <Typography variant='p' color='secondary'>Bienvenido</Typography>
+      </div>
+      <div className='cardSaldo' style={{ color: '#f1f0ea', display: 'flex', flexDirection: 'column', gap: '1rem', width: '324px', heigt: '160px', background: '#8d4eb5', padding: '16px', borderRadius: '10px' }}>
+        <div>
+          <div style={{ fontSize: '14px' }}>Cuentas</div>
+          <div style={{ fontSize: '12px' }}>Saldos totales</div>
         </div>
-        <div className='text-wrapper-10' style={{ alignSelf: 'center' }}>Cuentas</div>
-        <div className='text-wrapper-9' style={{ alignSelf: 'center' }}>Saldos totales</div>
-        <div className='text-wrapper-11' style={{ alignSelf: 'center' }}>Tu CVU</div>
+        <div style={{ display: 'flex', fontSize: '28px', fontWeight: '400' }}>
+          <span>$ </span>
+          <span>0,00</span>
+        </div>
+        <div style={{ fontSize: '12px' }}>Tu CVU</div>
       </div>
     </section>
   )
