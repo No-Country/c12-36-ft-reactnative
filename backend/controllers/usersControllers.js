929af5bc4bb86c9b3a9867976facd60b3f69f247
Masiel <masiel31@msn.com>
2023-07-11 10:26:15 -0300

    Validaciones y Encriptacion Password

--- backend/controllers/usersControllers.js
+++ backend/controllers/usersControllers.js
@@ -1,5 +1,6 @@
 const Users = require("../models/user.model");
 require("dotenv").config();
+const bcrypt = require ('bcrypt')
 
 //Obtener todos los usuario
 const getUsers = async (req, res) => {
@@ -24,50 +25,53 @@ const getUserById = async (req, res) => {
 
 //Crear Usuario
 const createUser = async (req, res) => {
-  try {
-    const newUser = {
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      email: req.body.email,
-      password: req.body.password,
-      isActivated: req.body.isActivated || false,
-    };
-    const user = Users.create(newUser);
-    res
-      .status(201)
-      .send({ mensaje: "Usuario creado con exito", usuario: newUser });
-  } catch (error) {
-    res.status(404).send(error);
-  }
-};
+    const salt = bcrypt.genSaltSync(10); //cantidad de saltos que da para encriptar, entre mas vuelta da es mas segura.
+    const passwordHash = bcrypt.hashSync(req.body.password, salt);
+    try {
+      const newUser = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: passwordHash,
+        isActivated: req.body.isActivated || false,
+      };
+      const user = Users.create(newUser);
+      res
+        .status(201)
+        .send({ mensaje: "Usuario creado con exito", usuario: newUser });
+    } catch (error) {
+      res.status(404).send(error);
+    }
+  };
 
 //Modificar usuario
 const editUser = async (req, res) => {
-  const id = req.params.id;
-  const userEdited = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    isActivated: req.body.isActivated,
-    dateOfBirth: req.body.dateOfBirth,
-    dni: req.body.dni,
-    nacionality:  req.body.nacionality,
-    address: {
+    const id = req.params.id;
+    const salt = bcrypt.genSaltSync(10);
+    const userEdited = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      dateOfBirth: req.body.dateOfBirth,
+      dni: req.body.dni,
+      nacionality: req.body.nacionality,
+      address: {
         street: req.body.address.street,
         number: req.body.address.number,
         zipcode: req.body.address.zipcode
       },
-  
-  };
-  try {
-    const user = await Users.findByIdAndUpdate(id, userEdited);
-    res
-      .status(200)
-      .send({ mensaje: "Usuario modificado con éxito", user: userEdited });
-  } catch (error) {
-    res.status(404).send(error);
+      isActivated: req.body.isActivated,
+      
+    };
+    try {
+      const user = await Users.findByIdAndUpdate(id, userEdited);
+      res
+        .status(200)
+        .send({ mensaje: "Usuario modificado con éxito", user: userEdited });
+    } catch (error) {
+      res.status(404).send(error);
+    }
   }
-};
 
 //Eliminar usuario
 const deleteUser = async (req, res) => {
