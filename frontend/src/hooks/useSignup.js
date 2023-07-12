fa4b40d77e5c1b491d3e5f87da49e5c979c9f2f6
unknown <cataniafrancodev@gmail.com>
2023-07-12 16:57:04 -0300

    dummy useSignUp

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,37 +1,22 @@
-import { useState } from 'react'
 import { useAuthContext } from './useAuthContext'
 
 export const useSignup = () => {
-  const [error, setError] = useState(null)
-  const [isLoading, setIsLoading] = useState(null)
   const { dispatch } = useAuthContext()
 
-  const signup = async (email, password) => {
-    setIsLoading(true)
-    setError(null)
-
-    const response = await fetch('/api/user/signup', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ email, password })
-    })
-    const json = await response.json()
-
-    if (!response.ok) {
-      setIsLoading(false)
-      setError(json.error)
+  const signup = async ({ email, firstName, lastName }) => {
+    const json = {
+      email,
+      firstName,
+      lastName,
+      token: 'akjsdfljasdlfadsjlfas' // Fake
     }
-    if (response.ok) {
-      // save the user to local storage
-      localStorage.setItem('user', JSON.stringify(json))
 
-      // update the auth context
-      dispatch({ type: 'LOGIN', payload: json })
+    // save the user to local storage
+    localStorage.setItem('user', JSON.stringify(json))
 
-      // update loading state
-      setIsLoading(false)
-    }
+    // update the auth context
+    dispatch({ type: 'LOGIN', payload: json })
   }
 
-  return { signup, isLoading, error }
+  return signup
 }
