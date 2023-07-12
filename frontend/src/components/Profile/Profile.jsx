2a58e211670c1cd1469603772797c29b5160115b
Joselias <joselias.dev@gmail.com>
2023-07-12 19:47:25 -0300

    fix: styles profile

--- frontend/src/components/Profile/Profile.jsx
+++ frontend/src/components/Profile/Profile.jsx
@@ -3,9 +3,7 @@ import Button from '@mui/material/Button'
 import React, { useState } from 'react'
 import TextField from '@mui/material/TextField'
 import FormLabel from '@mui/material/FormLabel'
-import RadioGroup from '@mui/material/RadioGroup'
-import FormControlLabel from '@mui/material/FormControlLabel'
-import Radio from '@mui/material/Radio'
+import './profile.css'
 import Select from '@mui/material/Select'
 import MenuItem from '@mui/material/MenuItem'
 import provincias from '../../data/provincias.json'
@@ -16,62 +14,77 @@ import dayjs from 'dayjs'
 import customParseFormat from 'dayjs/plugin/customParseFormat'
 import { es } from 'dayjs/locale/es'
 import localeData from 'dayjs/plugin/localeData'
+import picture from '../../assets/picture.svg'
+import camera from '../../assets/fi-sr-camera.png'
+import BtnGradient from '../BtnGradient/BtnGradient'
 
 const Profile = () => {
   dayjs.locale('es')
   dayjs.extend(localeData)
   dayjs.extend(customParseFormat)
 
-  const [provincia, setProvincia] = useState(1)
+  /*   const [provincia, setProvincia] = useState(1) */
   const [date, setDate] = useState(dayjs())
 
-  const handleChange = (event) => {
+  /*   const handleChange = (event) => {
     setProvincia(event.target.value)
-  }
-
+  } */
+  const changes = { text: 'Guardar cambios' }
   return (
-    <section>
+    <section style={{ width: '50%', margin: 'auto', display: 'flex', flexDirection: 'column', gap: '69px' }}>
       <div>
-        <p>J</p>
-        <div>
+        <div className='circlePicture'>
+          <img className='pictureProfile' src={picture} alt=' ' />
+          <div className='pictureCamera'>
+            <img className='camera' src={camera} alt='' />
+          </div>
+
+        </div>
+
+        {/* <div>
           <Button>Subir Foto</Button>
           <Button>Quitar</Button>
-        </div>
+        </div> */}
       </div>
-      <FormControl>
-        <div>
-          <FormLabel id='firstName'>Nombres* <TextField id='firstName' label='Francisco' /></FormLabel>
-          <FormLabel id='lastname'>Apellidos* <TextField id='lastname' label='Ledesma' /></FormLabel>
+      <FormControl style={{ gap: '34px' }}>
+        <div className='containerLabel'>
+          <FormLabel className='labelInput' id='firstName'>Nombres* <TextField id='firstName' placeholder='Francisco' /></FormLabel>
+          <FormLabel className='labelInput' id='lastname'>Apellidos* <TextField id='lastname' placeholder='Ledesma' /></FormLabel>
         </div>
-        <div>
-          <FormLabel id='email'>Correo electronico* <TextField id='email' label='Franciscoledesma@gmail.com' /></FormLabel>
-          <FormLabel id='nacionality'>Nacionalidad* <TextField id='nacionality' label='Argentina' /></FormLabel>
+        <div className='containerLabel'>
+          <FormLabel className='labelInput' id='email'>Correo electronico* <TextField id='email' placeholder='Franciscoledesma@gmail.com' /></FormLabel>
+          <FormLabel className='labelInput' id='nacionality'>Nacionalidad* <TextField id='nacionality' placeholder='Argentina' /></FormLabel>
         </div>
-        <div>
-          <FormLabel id='province'>Provincia*</FormLabel>
+        <div className='containerLabel'>
+          <div className='labelInput'>
+            <FormLabel style={{ color: '#f1f0ea' }} id='dateOfBirth'>Fecha de Nacimiento*</FormLabel>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DatePicker defaultValue={date} onChange={(newDate) => setDate(newDate.format('DD/MM/YYYY'))} />
+            </LocalizationProvider>
+          </div>
+          <FormLabel className='labelInput' id='dni'>DNI*<TextField id='dni' placeholder='12345678' /></FormLabel>
+        </div>
+
+        <div className='containerLabel'>
+          <FormLabel className='labelInput' id='address'>Dirección* <TextField id='address' placeholder='Calle' /></FormLabel>
+          <FormLabel className='labelInput' id='number'>Número* <TextField id='number' placeholder='123' /></FormLabel>
+
+        </div>
+        <div className='containerLabel'>
+          {/* <FormLabel id='province'>Provincia*</FormLabel>
           <Select id='province' value={provincia} displayEmpty onChange={handleChange}>
             {
                   provincias.map(provincia => (
                     <MenuItem value={provincia.id} key={provincia.id}>{provincia.nombre}</MenuItem>
                   ))
             }
-          </Select>
-          <FormLabel id='zipCode'>Codigo Postal* <TextField id='zipCode' label='1000' /></FormLabel>
-        </div>
-        <div>
-          <FormLabel id='address'>Dirección* <TextField id='address' label='Calle' /></FormLabel>
-          <FormLabel id='number'>Número* <TextField id='number' label='123' /></FormLabel>
-
-        </div>
-        <div>
-          <FormLabel id='dateOfBirth'>Fecha de Nacimiento*</FormLabel>
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DatePicker defaultValue={date} onChange={(newDate) => setDate(newDate.format('DD/MM/YYYY'))} />
-          </LocalizationProvider>
-          <FormLabel id='dni'>DNI*<TextField id='dni' label='12345678' /></FormLabel>
+          </Select> */}
+          <FormLabel className='labelInput' id='zipCode'>Codigo Postal* <TextField id='zipCode' placeholder='1000' /></FormLabel>
         </div>
         <div />
+        <BtnGradient prop={changes} />
       </FormControl>
+
     </section>
   )
 }
