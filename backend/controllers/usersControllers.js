74f34f3b91c846175f7781cdf96d538e82cee30b
Masiel <masiel31@msn.com>
2023-07-14 16:56:17 -0300

    Agregando Payload en Login y en register modificando respuesta al front

--- backend/controllers/usersControllers.js
+++ backend/controllers/usersControllers.js
@@ -1,7 +1,7 @@
 const Users = require("../models/user.model");
 require("dotenv").config();
-const bcrypt = require ('bcrypt')
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 //Obtener todos los usuario
 const getUsers = async (req, res) => {
@@ -26,66 +26,76 @@ const getUserById = async (req, res) => {
 
 //Crear Usuario
 const createUser = async (req, res) => {
+  // Verificando si el usuario existe
+  const email = req.body.email;
+  const existingUser = await Users.findOne({ email });
+  if (existingUser) {
+    return res
+      .status(400)
+      .json({
+        message: "Ya existe un usuario con el mismo correo electrónico",
+      });
+  }
 
-    // Verificando si el usuario existe
-    const email = req.body.email;
-    const existingUser = await Users.findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({ message: 'Ya existe un usuario con el mismo correo electrónico' });
-    }
-    
-    const salt = bcrypt.genSaltSync(10); //cantidad de saltos que da para encriptar, entre mas vuelta da es mas segura.
-    const passwordHash = bcrypt.hashSync(req.body.password, salt);
-    try {
-      const newUser = {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        password: passwordHash,
-        isActivated: req.body.isActivated || false,
-      };
-      const user = await Users.create(newUser);
-          const accessToken = jwt.sign({ id: user.email }, process.env.SECRET_KEY, {
-          expiresIn: process.env.JWT_EXPIRE_REGISTER,
-      });                                                                           
-     
-      //Enviar una respuesta al cliente
-      res.status(200).json({ usuario: newUser, accessToken, mensaje: "Usuario creado con exito" });                             // descomentar para el token
-      
-      
-    } catch (error) {
-      res.status(404).send(error);
-    }
-  };
-
-//Modificar usuario
-const editUser = async (req, res) => {
-    const id = req.params.id;
-    const salt = bcrypt.genSaltSync(10);
-    const userEdited = {
+  const salt = bcrypt.genSaltSync(10); //cantidad de saltos que da para encriptar, entre mas vuelta da es mas segura.
+  const passwordHash = bcrypt.hashSync(req.body.password, salt);
+  try {
+    const newUser = {
       firstName: req.body.firstName,
       lastName: req.body.lastName,
       email: req.body.email,
-      dateOfBirth: req.body.dateOfBirth,
-      dni: req.body.dni,
-      nacionality: req.body.nacionality,
-      address: {
-        street: req.body.address.street,
-        number: req.body.address.number,
-        zipcode: req.body.address.zipcode
-      },
-      isActivated: req.body.isActivated,
-      
+      password: passwordHash,
+      isActivated: req.body.isActivated || false,
     };
-    try {
-      const user = await Users.findByIdAndUpdate(id, userEdited);
-      res
-        .status(200)
-        .send({ mensaje: "Usuario modificado con éxito", user: userEdited });
-    } catch (error) {
-      res.status(404).send(error);
-    }
+    const user = await Users.create(newUser);
+    const accessToken = jwt.sign({ id: user.email }, process.env.SECRET_KEY, {
+      expiresIn: process.env.JWT_EXPIRE_REGISTER,
+    });
+
+    //Enviar una respuesta al cliente
+    res
+      .status(200)
+      .json({
+        mensaje: "Usuario creado con exito",
+        usuario: {
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          email: req.body.email,
+        },
+        accessToken,
+      }); // descomentar para el token
+  } catch (error) {
+    res.status(404).send(error);
   }
+};
+
+//Modificar usuario
+const editUser = async (req, res) => {
+  const id = req.params.id;
+  const salt = bcrypt.genSaltSync(10);
+  const userEdited = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    dateOfBirth: req.body.dateOfBirth,
+    dni: req.body.dni,
+    nacionality: req.body.nacionality,
+    address: {
+      street: req.body.address.street,
+      number: req.body.address.number,
+      zipcode: req.body.address.zipcode,
+    },
+    isActivated: req.body.isActivated,
+  };
+  try {
+    const user = await Users.findByIdAndUpdate(id, userEdited);
+    res
+      .status(200)
+      .send({ mensaje: "Usuario modificado con éxito", user: userEdited });
+  } catch (error) {
+    res.status(404).send(error);
+  }
+};
 
 //Eliminar usuario
 const deleteUser = async (req, res) => {
@@ -102,32 +112,44 @@ const deleteUser = async (req, res) => {
 
 //Login usuario
 
-const loginUser = async(req, res)=>{
+const loginUser = async (req, res) => {
   try {
-      const email = req.body.email
-      const userFind = await Users.findOne({email})
-      if(userFind){
-          const passwordEnterByUser = req.body.password
-          const passwordStoredInDB = userFind.password
-          const passwordMatch = bcrypt.compareSync(passwordEnterByUser, passwordStoredInDB)
-          if(passwordMatch){
-
-
-         const accessToken = jwt.sign({ id: userFind.email }, process.env.SECRET_KEY, {
-          expiresIn: process.env.JWT_EXPIRE_LOGIN,
-      });                                                                         
-      // Enviar una respuesta al cliente
-       res.status(200).json({ accessToken, mensaje: "Usuario logueado con éxito" });                                  
-                
-                  
-          }else{
-              res.status(400).send({ mensaje: "Email o Contraseña incorrectos " })
+    const email = req.body.email;
+    const userFind = await Users.findOne({ email });
+    if (userFind) {
+      const passwordEnterByUser = req.body.password;
+      const passwordStoredInDB = userFind.password;
+      const passwordMatch = bcrypt.compareSync(
+        passwordEnterByUser,
+        passwordStoredInDB
+      );
+      if (passwordMatch) {
+        const payload = {
+          email: userFind.email,
+          firstName: userFind.firstName,
+          lastName: userFind.lastName,
+        };
+        const accessToken = jwt.sign(
+          { id: userFind.email },
+          process.env.SECRET_KEY,
+          {
+            expiresIn: process.env.JWT_EXPIRE_LOGIN,
           }
+        );
+        // Enviar una respuesta al cliente
+        res
+          .status(200)
+          .json({
+            mensaje: "Usuario logueado con éxito",
+            ...payload,
+            accessToken,
+          });
+      } else {
+        res.status(400).send({ mensaje: "Email o Contraseña incorrectos " });
       }
-
-
+    }
   } catch (error) {
-      res.send(error)
+    res.send(error);
   }
 };
 
@@ -152,7 +174,6 @@ const resetPassword = async (req, res) => {
     res.send(error);
   }
 };
-
 
 module.exports = {
   loginUser,
--- backend/data/dbConnection.js
+++ backend/data/dbConnection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 require ('dotenv').config();
 
 const initDBConnection = ()=>{
-    const DB_CONNECTION_STRING = process.env.DB_URL_CONNECTION;
+    const DB_CONNECTION_STRING = process.env.DB_URL_CONNECTION ;
     mongoose.set('strictQuery', false)
     mongoose.connect(DB_CONNECTION_STRING)
     .then(()=>{
