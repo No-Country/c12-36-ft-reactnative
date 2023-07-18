013079b6d0b8563cdfd9cd3244bd4d56b54b892a
unknown <cataniafrancodev@gmail.com>
2023-07-18 17:22:24 -0300

    sharedProperties

--- frontend/src/containers/Profile.jsx
+++ frontend/src/containers/Profile.jsx
@@ -46,121 +46,120 @@ const Profile = () => {
     }
   }
 
+  const sharedProperties = {
+    color: 'secondary',
+    autoComplete: 'off',
+    sx: customSx
+  }
+
   return (
-    <div className='signup-form'>
-      <main>
-        <section style={{ width: '60%', display: 'flex', flexDirection: 'column' }}>
-          <FormControl>
-            <div className='textField'>
-              <FormLabel
-                id='email'
-                className='labelInput'
-              >
-                Correo electrónico*
-                <TextField
-                  id='email'
-                  aria-invalid={errors.email ? 'true' : 'false'}
-                  color='secondary'
-                  autoComplete='off'
-                  sx={customSx}
-                  {
-                    ...register('email', { required: true, pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/ })
-                  }
-                />
-                {
-                  errors.email && errors.email.type === 'required' &&
-                    <p className='error' role='alert'>*El email no puede estar vacío</p>
-                }
-                {
-                  errors.email && errors.email.type === 'pattern' &&
-                    <p className='error' role='alert'>*Email inválido</p>
-                }
-              </FormLabel>
-            </div>
+    <div style={{ padding: '20px' }}>
+      <FormControl style={{ gap: '34px' }}>
+        <div className='containerLabel'>
+          <FormLabel
+            id='firstName'
+            className='labelInput'
+          >
+            Nombres*
+            <TextField
+              id='firstName'
+              aria-invalid={errors.firstName ? 'true' : 'false'}
+              {...sharedProperties}
+              {
+                ...register('firstName', { required: true, minLength: 3 })
+              }
+            />
+            {
+              errors.firstName && errors.firstName.type === 'required' &&
+                <p className='error' role='alert'>*Este campo no puede estar vacío</p>
+            }
+            {
+              errors.firstName && errors.firstName.type === 'pattern' &&
+                <p className='error' role='alert'>*Nombre inválido</p>
+            }
+          </FormLabel>
+          <FormLabel
+            id='email'
+            className='labelInput'
+          >
+            Apellidos*
+            <TextField
+              id='lastName'
+              aria-invalid={errors.lastName ? 'true' : 'false'}
+              {...sharedProperties}
+              {
+                ...register('lastName', { required: true, minLength: 3 })
+              }
+            />
+            {
+              errors.lastName && errors.lastName.type === 'required' &&
+                <p className='error' role='alert'>*Este campo no puede estar vacío</p>
+            }
+            {
+              errors.lastName && errors.lastName.type === 'pattern' &&
+                <p className='error' role='alert'>*Apellido inválido</p>
+            }
+          </FormLabel>
+        </div>
+        <div className='containerLabel'>
+          <FormLabel
+            id='email'
+            className='labelInput'
+          >
+            Correo electrónico*
+            <TextField
+              id='email'
+              aria-invalid={errors.email ? 'true' : 'false'}
+              {...sharedProperties}
+              {
+                ...register('email', { required: true, pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/ })
+              }
+            />
+            {
+              errors.email && errors.email.type === 'required' &&
+                <p className='error' role='alert'>*Este campo no puede estar vacío</p>
+            }
+            {
+              errors.email && errors.email.type === 'pattern' &&
+                <p className='error' role='alert'>*Email inválido</p>
+            }
+          </FormLabel>
+          <FormLabel
+            id='email'
+            className='labelInput'
+          >
+            Nacionalidad*
+            <TextField
+              id='nacionality'
+              aria-invalid={errors.nacionality ? 'true' : 'false'}
+              {...sharedProperties}
+              {
+                ...register('nacionality', { required: true })
+              }
+            />
+            {
+              errors.nacionality && errors.nacionality.type === 'required' &&
+                <p className='error' role='alert'>*Este campo no puede estar vacío</p>
+            }
+          </FormLabel>
+        </div>
 
-            <div className='textField' style={{ gap: '16px' }}>
-              <div>
-                <TextField
-                  id='firstName'
-                  label='Nombre'
-                  variant='outlined'
-                  aria-invalid={errors.firstName ? 'true' : 'false'}
-                  color='secondary'
-                  autoComplete='off'
-                  sx={{
-                    input: {
-                      color: '#fdfdfe',
-                      fontSize: '1rem'
-                    },
-                    label: {
-                      color: 'gray'
-                    }
-                  }}
-                  {
-                  ...register('firstName', { required: true, minLength: 3 })
-                  }
-                />
-                {
-                  errors.firstName && errors.firstName.type === 'required' &&
-                    <p className='error' role='alert'>*El nombre es obligatorio</p>
-                }
-                {
-                  errors.firstName && errors.firstName.type === 'minLength' &&
-                    <p className='error' role='alert'>*Mínimo 3 caracteres</p>
-                }
-              </div>
-
-              <div>
-                <TextField
-                  id='lastName'
-                  label='Apellido'
-                  variant='outlined'
-                  aria-invalid={errors.lastName ? 'true' : 'false'}
-                  color='secondary'
-                  autoComplete='off'
-                  sx={{
-                    input: {
-                      color: '#fdfdfe',
-                      fontSize: '1rem'
-                    },
-                    label: {
-                      color: 'gray'
-                    }
-                  }}
-                  {
-                  ...register('lastName', { required: true, minLength: 3 })
-                  }
-                />
-                {
-                  errors.lastName && errors.lastName.type === 'required' &&
-                    <p className='error' role='alert'>*El apellido es obligatorio</p>
-                }
-                {
-                  errors.lastName && errors.lastName.type === 'minLength' &&
-                    <p className='error' role='alert'>*Mínimo 3 caracteres</p>
-                }
-              </div>
-            </div>
-
-            {isLoading && <CircularProgress color='secondary' />}
-            <p className='response-error'>{error}</p>
-
-            <article className='signup-button'>
-              <Button
-                className='btnGradient'
-                variant='contained'
-                type='submit'
-                sx={{
-                  color: '#F1F0EA'
-                }}
-                onClick={handleSubmit(onSubmit)}
-              >
-                Guardar cambios
-              </Button>
-            </article>
-          </FormControl>
-        </section>
-      </main>
+        {isLoading && <CircularProgress color='secondary' />}
+        <p className='response-error'>{error}</p>
+        <article className='signup-button'>
+          <Button
+            className='btnGradient'
+            variant='contained'
+            type='submit'
+            sx={{
+              color: '#F1F0EA'
+            }}
+            onClick={handleSubmit(onSubmit)}
+          >
+            Guardar cambios
+          </Button>
+        </article>
+      </FormControl>
     </div>
   )
 }
