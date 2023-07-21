864290cc8b957e936219deb4238ceb4424568cc3
Masiel <masiel31@msn.com>
2023-07-21 16:18:55 -0300

    Transactions Routes, Validatios and Controller-Get, GetiID, PostCreateTransaction

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,39 +1,42 @@
-const { Schema, model } = require("mongoose")
+const { Schema, model } = require("mongoose");
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     firstName: {
-        type: String, 
-        required: true
-    }, 
-
-    lastName: {
-        type: String, 
-        required: true
+      type: String,
+      required: true,
     },
 
-    email:{
-        type: String,
-        required: true,
-        unique: true
+    lastName: {
+      type: String,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
 
     password: {
-        type: String, 
-        required: true
-    }, 
-   
+      type: String,
+      required: true,
+    },
 
     dateOfBirth: Date,
     dni: Number,
     nacionality: String,
-    address: {
-        street: String,
-        number: Number,
-        zipcode: String
-    },
-      isActivated: Boolean,
-}, /* {
-    timestamps:true
-} */)
 
-module.exports = model("User", userSchema)
+    address: {
+      street: String,
+      number: Number,
+      zipcode: String,
+    },
+    
+    isActivated: Boolean,
+  } /* {
+    timestamps:true
+} */
+);
+
+module.exports = model("User", userSchema);
