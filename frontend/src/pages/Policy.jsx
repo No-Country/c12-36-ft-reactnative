6c275213fc3b2a3d8f4e71769d0a6c3744670e43
TiagoAlvarezSchiaffino <t.alvarezschiaffino@gmail.com>
2023-07-22 20:30:28 -0300

    Footerc/TermsPages

---
+++ frontend/src/pages/Policy.jsx
@@ -0,0 +1,104 @@
+import React from 'react'
+import Typography from '@mui/material/Typography'
+import '../styles/policy.css'
+
+const Policy = () => {
+  return (
+    <div className='policyContainer'>
+      <div className='header'>
+        <Typography variant='h4'>Política de Privacidad de PoketPal Argentina</Typography>
+        <Typography variant='subtitle1'>Fecha de entrada en vigencia: [Viernes 23 de junio de 2023]</Typography>
+        <Typography variant='body1'>Cuando navegás por la web, dejás una huella digital que es la información de tu dispositivo y la forma en que lo usás cuando estás en línea. La mayoría de los sitios web recaban esta información. Queremos informarte que nosotros (PoketPal, proveedores de tecnología, empresas de ciberseguridad, socios de redes sociales y publicitarios) también recopilamos esta información para poder ofrecerte una mejor experiencia en nuestro sitio.</Typography>
+      </div>
+      <div className='content'>
+        <Typography variant='h5'>Aviso de sitio web público</Typography>
+        <Typography variant='body1'>Este aviso cubre partes de este sitio web donde no necesita una contraseña (como esta página).</Typography>
+        <Typography variant='h5'>Aviso de cookies</Typography>
+        <Typography variant='body1'>Acá te contamos cómo recopilamos información de tu actividad en nuestro sitio web desde tu navegador o dispositivo móvil.</Typography>
+        <Typography variant='h5'>Información personal recopilada por este sitio</Typography>
+        <Typography variant='body1'>Recopilamos información cuando navegás nuestro sitio y también cuando visitás una de nuestras páginas de redes sociales. Lo que podemos ver es:</Typography>
+        <ul>
+          <li>1. Tu dispositivo y conexión a Internet</li>
+          <li>2. Tipo de dispositivo que estás utilizando</li>
+          <li>3. Dirección IP y detalles sobre tu conexión</li>
+          <li>4. Tamaño de tu pantalla</li>
+          <li>5. Navegador web que estás usando</li>
+        </ul>
+        <Typography variant='body1'>Haremos un seguimiento desde el inicio de tu navegación, sobre el flujo de la visita en el sitio, cuál fue la última página visitada. Cuando completes un formulario de solicitud o consulta, recopilemos la información que nos proporcione.</Typography>
+        <Typography variant='h5'>Tecnología de filtrado de ciberseguridad</Typography>
+        <Typography variant='body1'>Esto mantiene nuestro sitio web a salvo de fraudes y crimen.</Typography>
+        <Typography variant='h5'>Cookies y tecnología similar</Typography>
+        <Typography variant='body1'>Estos los configuramos en su dispositivo y nuestros socios de confianza. En un primer ingreso no sabemos de tu actividad en nuestro sitio, ni los espacios de interés. Cuando comenzamos a recopilar información de navegación podemos ir entendiendo necesidades y posibles ofrecimientos. Por ejemplo:</Typography>
+        <ul>
+          <li>1. Si navegás por nuestro sitio web y luego accedés a CUENTAS, la próxima vez que nos visites, podremos mostrar en la página de inicio contenidos referentes exclusivamente a un cliente.</li>
+          <li>2. Si navegás por nuestro sitio web mientras estás conectado a las redes sociales, éstas pueden conectar esa información y asociarla a tu perfil. Es posible que tus datos de navegación en nuestro sitio queden en poder de terceros.</li>
+        </ul>
+        <Typography variant='body1'>Sólo usamos tus datos cuando es de nuestro interés legítimo. Esto significa que debemos tener una razón válida y legal para usar tus datos y equilibrar esto con tus intereses.</Typography>
+        <Typography variant='h5'>Cómo la usamos</Typography>
+        <Typography variant='body1'>Usamos su información para operar el sitio web y monitorear qué tan bien está funcionando en cuanto a si los contenidos son consultados y relevantes para el común de los usuarios. La mayoría de los sitios web hacen esto. Es con el objetivo de mejorar nuestras ofertas hacia vos. Usamos tu información para:</Typography>
+        <ul>
+          <li>1. Comprender cómo utilizás nuestros servicios.</li>
+          <li>2. Ver las tendencias en la oferta de productos.</li>
+          <li>3. Desarrollar ofertas y productos bien focalizados.</li>
+          <li>4. Encontrar ofertas y productos que puedan ser de tu interés.</li>
+          <li>5. Mejorar nuestro servicio.</li>
+          <li>6. Medir la efectividad de nuestras campañas de marketing en pos de una mejora continua.</li>
+        </ul>
+        <Typography variant='body1'>Protección de seguridad: Usamos tu información para ayudar a protegerte a vos y a nosotros mismos contra fraude o delito. Por ejemplo, nuestros proveedores de servicios miran todos los usos de nuestro sitio web para ayudar a bloquear la actividad maliciosa. Esto te ayuda a mantenerte a vos y nosotros a salvo.</Typography>
+        <Typography variant='h5'>Publicidad online</Typography>
+        <Typography variant='body1'>Cuando hacemos publicidad en línea, podemos compartir la información de tu actividad en el sitio con nuestros socios publicitarios. Podemos pedirle a nuestro socio publicitario que utilice esa información para:</Typography>
+        <ul>
+          <li>1. Que compruebe si tenés una cuenta con ellos.</li>
+          <li>2. Que seleccione qué anuncios enviarte.</li>
+          <li>3. Excluirte de anuncios que no son relevantes para vos, (Ej. si el anuncio es para un servicio ya tenés).</li>
+          <li>4. Enviar anuncios a personas que tengan un perfil similar al tuyo o compartan tus intereses.</li>
+          <li>5. Identificar qué otro tipo de oferta puede interesarte.</li>
+          <li>6. Entender la efectividad de nuestras campañas de marketing y mejorarlas.</li>
+        </ul>
+        <Typography variant='h5'>Medios de comunicación social</Typography>
+        <Typography variant='body1'>PoketPal usa las redes sociales para:</Typography>
+        <ul>
+          <li>1. Conectarse con vos.</li>
+          <li>2. Compartir información sobre nuestros productos y servicios.</li>
+        </ul>
+        <Typography variant='body1'>Recordá que las plataformas de redes sociales pueden permitirte elegir qué publicidad querés recibir. Cuando navegás un contenido de PoketPal en Facebook, tanto Facebook como PoketPal recopilan información tuya. Esto incluye:</Typography>
+        <ul>
+          <li>1. En dónde hacés click.</li>
+          <li>2. Si iniciás un chat en Messenger (no el contenido del mismo).</li>
+          <li>3. Qué tipo de contenidos ves.</li>
+          <li>4. Cuando pasas el cursor sobre un link o un evento en la pantalla.</li>
+          <li>5. Qué decís (comentarios o reacciones).</li>
+          <li>6. Tus acciones.</li>
+          <li>7. Qué compartís o recomendás.</li>
+          <li>8. Tu actividad.</li>
+          <li>9. Cuánto tiempo pasas en las páginas.</li>
+          <li>10. Fecha y hora de las acciones o sitios web que visitaste.</li>
+          <li>11. Tu ubicación (país o región, no es tu ubicación precisa a menos que la hayas proporcionado en tu perfil de usuario y hayas iniciado sesión).</li>
+          <li>12. Tipo de dispositivo.</li>
+          <li>13. Tipo de conexión a internet.</li>
+          <li>14. Tu perfil de Facebook.</li>
+          <li>15. ID de usuario.</li>
+        </ul>
+        <Typography variant='body1'>HSBC y Facebook tienen acceso a esta información para utilizarla en informes y con fines de marketing. Esto ayuda a PoketPal a mejorar nuestra oferta en Facebook y a crear mejores y más efectivas campañas de marketing para hacerte llegar una oferta más relevante.</Typography>
+        <Typography variant='body1'>Facebook es un "controlador conjunto" junto a nosotros en el procesamiento de datos donde recopilamos información sobre el comportamiento de los usuarios:</Typography>
+        <ul>
+          <li>1. Tus acciones en nuestra página de Facebook.</li>
+          <li>2. A través del píxel de Facebook en nuestro sitio web.</li>
+        </ul>
+        <Typography variant='body1'>Con Facebook acordamos compartir responsabilidades sobre cómo proteger tus datos personales, mediante:</Typography>
+        <ul>
+          <li>1. Asegurarse de que cada uno tenga una base legal para el procesamiento conjunto.</li>
+          <li>2. Honrando tus derechos legales respecto a tus datos.</li>
+          <li>3. Garantizando la seguridad del procesamiento conjunto.</li>
+        </ul>
+        <Typography variant='body1'>Podés contactar a Facebook para saber más sobre este proceso. Esto incluye el ejercicio de tus derechos con respecto a los datos que Facebook recopila y conserva. Más detalles sobre cómo Facebook procesa tu información personal, la base legal en la que se basa, tus derechos y los datos de contacto de Facebook: <a href='https://www.facebook.com/about/privacy' target='_blank' rel='noopener noreferrer'>facebook.com/about/privacy</a>.</Typography>
+        <Typography variant='h5'>Seguridad</Typography>
+        <Typography variant='body1'>Utilizamos una gran variedad de medidas para mantener tu información segura, como el cifrado de los datos.</Typography>
+        <Typography variant='h5'>Almacenamiento</Typography>
+        <Typography variant='body1'>Mantenemos tu información de acuerdo con nuestra política de retención de datos, cumpliendo con los requisitos legales y reglamentarios para un uso con fines legítimos.</Typography>
+      </div>
+    </div>
+  )
+}
+
+export default Policy
