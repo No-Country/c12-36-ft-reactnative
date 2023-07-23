1c5e54a6faf82d7cf798a745f918774cb0aeaa2f
Angelica Rodriguez <85319178+angelicarm3@users.noreply.github.com>
2023-07-23 18:25:52 -0500

    footer responsive (#79)

--- frontend/src/containers/Footer.jsx
+++ frontend/src/containers/Footer.jsx
@@ -12,38 +12,38 @@ const Footer = () => {
           <ul className='footer_options'>
             <li><a href='terms-conditions' target='_blank'>Terminos y condiciones</a></li>
             <li><a href='privacy-policies' target='_blank'>Politicas de Privacidad</a></li>
-            <li><a href='defensa-consumidor' target='_blank'>Defensa al Consumidor</a></li>
+            <li><a href='page-not-found' target='_blank'>Defensa al Consumidor</a></li>
           </ul>
         </div>
         <div>
           <p className='lists_title'>Sobre PocketPal</p>
           <ul className='footer_options'>
             <li><a href='about-us' target='_blank'>Acerca de nosotros</a></li>
-            <li><a href='enviar-recibir-dinero' target='_blank'>Enviar y recibir dinero</a></li>
-            <li><a href='beneficios' target='_blank'>Beneficios</a></li>
+            <li><a href='page-not-found' target='_blank'>Enviar y recibir dinero</a></li>
+            <li><a href='page-not-found' target='_blank'>Beneficios</a></li>
           </ul>
         </div>
         <div>
           <p className='lists_title'>Ayuda y Seguridad</p>
           <ul className='footer_options'>
-            <li><a href='contacto' target='_blank'>Contacto</a></li>
-            <li><a href='preguntas-frecuentes' target='_blank'>Preguntas Frecuentes</a></li>
-            <li><a href='seguridad' target='_blank'>Seguridad</a></li>
+            <li><a href='page-not-found' target='_blank'>Contacto</a></li>
+            <li><a href='page-not-found' target='_blank'>Preguntas Frecuentes</a></li>
+            <li><a href='page-not-found' target='_blank'>Seguridad</a></li>
           </ul>
         </div>
         <div>
           <p className='lists_title'>Nuestras redes:</p>
           <div className='rrss'>
             <a className='socialIconBack' href='https://www.facebook.com/profile.php?id=100094393616559' target='_blank' rel='noreferrer'>
-              <img src={facebook} alt='facebook' height='44px' />
+              <img src={facebook} alt='facebook' />
             </a>
             <a className='socialIconBack' href='https://www.instagram.com' target='_blank' rel='noreferrer'>
-              <img src={instagram} alt='instagram' height='40px' />
+              <img src={instagram} alt='instagram' />
             </a>
           </div>
         </div>
       </div>
-      <Typography className='footer_rigths' variant='h6' color='secondary' style={{ fontWeight: '600' }}>© 2023 Pocketpal, Inc</Typography>
+      <div className='footer_rigths'>© 2023 Pocketpal, Inc</div>
     </section>
   )
 }
