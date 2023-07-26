8733e0fc82d65ae5d5144ab9c08b6781b45edbeb
Joselias <joselias.dev@gmail.com>
2023-07-26 18:23:40 -0300

    fix: conflicts

--- frontend/src/pages/Transfers.jsx
+++ frontend/src/pages/Transfers.jsx
@@ -44,6 +44,7 @@ const Transfers = () => {
     console.log(users)
     if (users.length > 0 && recipient !== '') {
       console.log(recipient)
+      // eslint-disable-next-line
       const search = users.filter((user) => user.dni == recipient)
       setThisUser(search)
     }
@@ -69,35 +70,26 @@ const Transfers = () => {
             <Typography color='secondary' variant='p' className='titleTransfer'>Transacciones</Typography>
             <Typography color='secondary' variant='p' className='subtitleTransfer'>Ingresa un monto:</Typography>
           </div>
-          <div
-            className='containerAmount'
-          >
+          <div className='containerAmount'>
             <div className='balance'>
-              <div style={{ display: 'flex', alignItems: 'center' }}>
-                <label id='amount' className='amount'>$</label>
-                <TextField
-                  id='amount' name='amount' type='number' className='sinBorde' onChange={(e) => setAmount(e.target.value)} variant='standard' placeholder='0' {...register('amount', {
-                    required: 'Ingrese un monto valido',
-                    pattern: {
-                      value: /^[0-9]+(\.[0-9]{1,2})?$/,
-                      message: 'Ingrese un monto válido (ej: 100 o 100.50).'
-                    }
-                  })}
-                  error={Boolean(errors.amount)}
-                  style={{
-                    fontSize: '6rem',
-                    '@media (maxWidth: 900px)': {
-                      fontSize: '3rem'
-                    }
-                  }}
-                />
-              </div>
-              {errors.amount && <p>{errors.amount.message}</p>}
-            </div>
-            <Typography variant='p' color='secondary' className='available'>Saldo disponible ${user.balance}</Typography>
-            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+              <label id='amount' className='amount'>$</label>
               <TextField
-                name='recipient' id='recipient' onChange={(e) => setRecipient(e.target.value)} label='Ingresar CBU o CVU' type='number' variant='standard' {
+                id='amount' name='amount' type='number' className='sinBorde' onChange={(e) => setAmount(e.target.value)} variant='standard' placeholder='0' {...register('amount', {
+                  required: 'Ingrese un monto valido',
+                  pattern: {
+                    value: /^[0-9]+(\.[0-9]{1,2})?$/,
+                    message: 'Ingrese un monto válido (ej: 100 o 100.50).'
+                  }
+                })}
+                error={Boolean(errors.amount)}
+              />
+            </div>
+            {errors.amount && <p className='transfer_error'>{errors.amount.message}</p>}
+
+            <Typography variant='p' color='secondary' className='available'>Saldo disponible ${user.balance}</Typography>
+            <div className='transfer_info'>
+              <TextField
+                className='transaction_input' name='recipient' id='recipient' onChange={(e) => setRecipient(e.target.value)} label='Ingresar CBU o CVU' type='number' variant='standard' {
               ...register('recipient', {
                 required: 'Ingrese un CVU valido',
                 minLength: 8,
@@ -106,30 +98,30 @@ const Transfers = () => {
             }
                 error={Boolean(errors.recipient)}
               />
-              {errors.recipient && <p>{errors.recipient.message}</p>}
-              {errors.recipient && errors.recipient.type === 'minLength' && <p>Ingrese un CBU o DNI</p>}
-              {errors.recipient && errors.recipient.type === 'maxLength' && <p>Ingrese un CBU o DNI</p>}
+              {errors.recipient && <p className='transfer_error'>{errors.recipient.message}</p>}
+              {errors.recipient && errors.recipient.type === 'minLength' && <p className='transfer_error'>Ingrese un CBU o DNI</p>}
+              {errors.recipient && errors.recipient.type === 'maxLength' && <p className='transfer_error'>Ingrese un CBU o DNI</p>}
               <div className='containerMotivo'>
-                <Typography variant='p' color='secondary'>Motivo:</Typography>
                 <TextField
-                  name='detail' id='detail' label='Varios' onChange={(e) => setDetail(e.target.value)} type='text' variant='standard' {
+                  className='transaction_input' name='detail' id='detail' label='Varios' onChange={(e) => setDetail(e.target.value)} type='text' variant='standard' {
               ...register('detail', {
                 required: 'Ingrese un motivo'
               })
             }
                   error={Boolean(errors.detail)}
                 />
-                <img src={pencil} alt='editar motivo' />
               </div>
-              {errors.detail && <p>{errors.detail.message}</p>}
+              {errors.detail && <p className='transfer_error'>{errors.detail.message}</p>}
             </div>
             <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '2rem' }}>
-              <button
-                className='btnGradient'
+              <Button
+                className='btnGradient transfer_button'
+                variant='text'
                 type='submit'
+                color='secondary'
               >
                 Continuar
-              </button>
+              </Button>
             </div>
 
           </div>
