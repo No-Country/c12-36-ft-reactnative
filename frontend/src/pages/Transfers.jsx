1468ae389ec3f3c8b8b71e71bbb805e15b2ce180
Angelica Rodriguez <85319178+angelicarm3@users.noreply.github.com>
2023-07-25 19:44:00 -0500

    Estilos transactions (#93)

--- frontend/src/pages/Transfers.jsx
+++ frontend/src/pages/Transfers.jsx
@@ -1,13 +1,13 @@
 import Typography from '@mui/material/Typography'
 import TextField from '@mui/material/TextField'
 import '../styles/transfer.css'
-/* import { useAuthContext } from '../hooks/useAuthContext'
- */import pencil from '../assets/fi-sr-pencil.png'
+/* import { useAuthContext } from '../hooks/useAuthContext' */
 import { useAuth } from '../hooks/useAuth'
 import { useForm } from 'react-hook-form'
 import { Button } from '@mui/material'
 import { transferRequest } from '../api/auth'
 import { useEffect, useState } from 'react'
+
 const Transfers = () => {
   const { user, authToken } = useAuth()
   const { register, formState: { errors }, handleSubmit } = useForm()
@@ -40,14 +40,18 @@ const Transfers = () => {
         <Typography color='secondary' variant='p' className='titleTransfer'>Transacciones</Typography>
         <Typography color='secondary' variant='p' className='subtitleTransfer'>Ingresa un monto:</Typography>
       </div>
-      <div
-        className='containerAmount'
-      >
-        <div className='balance'>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
+      <div className='containerAmount'>
+        <div>
+          <div className='balance'>
             <label id='amount' className='amount'>$</label>
             <TextField
-              id='amount' name='amount' type='number' className='sinBorde' onChange={(e) => setAmount(e.target.value)} variant='standard' placeholder='0' {...register('amount', {
+              className='sinBorde'
+              id='amount'
+              name='amount'
+              type='number'
+              onChange={(e) => setAmount(e.target.value)}
+              variant='standard'
+              placeholder='0' {...register('amount', {
                 required: 'Ingrese un monto valido',
                 pattern: {
                   value: /^[0-9]+(\.[0-9]{1,2})?$/,
@@ -55,20 +59,20 @@ const Transfers = () => {
                 }
               })}
               error={Boolean(errors.amount)}
-              style={{
-                fontSize: '6rem',
-                '@media (maxWidth: 900px)': {
-                  fontSize: '3rem'
-                }
-              }}
             />
           </div>
-          {errors.amount && <p>{errors.amount.message}</p>}
+          {errors.amount && <p className='transfer_error'>{errors.amount.message}</p>}
         </div>
         <Typography variant='p' color='secondary' className='available'>Saldo disponible ${user.balance}</Typography>
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <div className='transfer_info' style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
           <TextField
-            name='recipient' id='recipient' onChange={(e) => setCbuODni(e.target.value)} label='Ingresar CBU o CVU' type='number' variant='standard' {
+            className='transaction_input'
+            name='recipient'
+            id='recipient'
+            onChange={(e) => setCbuODni(e.target.value)}
+            label='Ingresar CBU o CVU'
+            type='number'
+            variant='standard' {
               ...register('recipient', {
                 required: 'Ingrese un CVU valido',
                 minLength: 8,
@@ -77,31 +81,38 @@ const Transfers = () => {
             }
             error={Boolean(errors.recipient)}
           />
-          {errors.recipient && <p>{errors.recipient.message}</p>}
+          {errors.recipient && <p className='transfer_error'>{errors.recipient.message}</p>}
           {errors.recipient && errors.recipient.type === 'minLength' && <p>Ingrese un CBU o DNI</p>}
           {errors.recipient && errors.recipient.type === 'maxLength' && <p>Ingrese un CBU o DNI</p>}
           <div className='containerMotivo'>
-            <Typography variant='p' color='secondary'>Motivo:</Typography>
             <TextField
-              name='detail' id='detail' label='Varios' onChange={(e) => setMotivo(e.target.value)} type='text' variant='standard' {
+              className='transaction_input'
+              name='detail'
+              id='detail'
+              label='Motivo'
+              onChange={(e) => setMotivo(e.target.value)}
+              type='text'
+              variant='standard' {
               ...register('detail', {
                 required: 'Ingrese un motivo'
               })
             }
               error={Boolean(errors.detail)}
             />
-            <img src={pencil} alt='editar motivo' />
           </div>
-          {errors.detail && <p>{errors.detail.message}</p>}
+          {errors.detail && <p className='transfer_error'>{errors.detail.message}</p>}
         </div>
         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '2rem' }}>
-          <button
-            className='btnGradient'
+          <Button
+            className='btnGradient transfer_button'
+            variant='text'
             type='submit'
-
+            sx={{
+              color: '#F1F0EA'
+            }}
           >
             Continuar
-          </button>
+          </Button>
         </div>
 
       </div>
