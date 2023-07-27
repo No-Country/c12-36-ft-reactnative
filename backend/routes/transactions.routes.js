88f230109fe918afeb56d31691c94d9fc7b8e2bd
Masiel <masiel31@msn.com>
2023-07-27 10:44:24 -0300

    Agregando Readmi Back

--- backend/routes/transactions.routes.js
+++ backend/routes/transactions.routes.js
@@ -1,23 +1,30 @@
-const Router = require('express')
-const { getTransactions, getTransactionById, createTransaction } = require('../controllers/transactionsController')
-const router = Router()
-const { body } = require('express-validator');
-const {validateErrors }= require('../middlewares/validateErrors');
-const {jwtValidation}= require('../middlewares/jwtvalidation');
+const Router = require("express");
+const {
+  getTransactions,
+  getTransactionById,
+  createTransaction,
+} = require("../controllers/transactionsController");
+const router = Router();
+const { body } = require("express-validator");
+const { validateErrors } = require("../middlewares/validateErrors");
+const { jwtValidation } = require("../middlewares/jwtvalidation");
 
+router.get("/", getTransactions);
 
-router.get('/', getTransactions)
+router.get("/:id", getTransactionById);
 
-router.get('/:id', getTransactionById);
+router.post(
+  "/",
+  body("recipient").notEmpty().withMessage("El DNI, CBU obligatorio"),
+  body("amount")
+    .notEmpty()
+    .withMessage("El valor es requerido")
+    .isNumeric()
+    .withMessage("El valor debe ser un número"),
+  body("detail").notEmpty().withMessage("El detalle es requerido."),
+  validateErrors,
+  jwtValidation,
+  createTransaction
+);
 
-router.post('/', 
-body('recipient').notEmpty().withMessage('El DNI, CBU obligatorio'),
-body('amount').notEmpty().withMessage('El valor es requerido').isNumeric().withMessage('El valor debe ser un número'),
-body('detail').notEmpty().withMessage('El detalle es requerido.'),
-validateErrors,
-jwtValidation,
-createTransaction,)
-
-// router.post('/', receiveTransaction )
-
-module.exports = router
+module.exports = router;
