6404f9cdeb5c1a26534689f27493a5fdca9ecd0d
angelicarm3 <angie00069@gmail.com>
2023-07-27 21:55:26 -0500

    correcciones estilo

--- frontend/src/containers/Sidebar.jsx
+++ frontend/src/containers/Sidebar.jsx
@@ -17,7 +17,7 @@ import { useNavigate } from 'react-router-dom'
  */
 const sidebarUp = [{ name: 'Mi panel', img: panel, path: '/home/dashboard', focus: true }, { name: 'Transacciones', img: tarjeta, path: '/home/transfers' }, { name: 'Movimientos', img: moves, path: '/home/history' }, { name: 'Mis datos', img: portrait, path: '/home/profile' }]
 // { name: 'Mis tarjetas', img: cards }, { name: 'Servicios', img: manos },
-const sidebarDown = [{ name: 'Terminos de uso', img: terms, path: 'home/terms-conditions' }, { name: 'Ayuda y soporte', img: help, path: 'home/contact-us' }, { name: 'Configuraciones', img: settings, path: '/home/settings/edit_profile' }]
+const sidebarDown = [{ name: 'Terminos de uso', img: terms, path: '/home/terms-conditions' }, { name: 'Ayuda y soporte', img: help, path: '/home/contact-us' }, { name: 'Configuraciones', img: settings, path: '/home/settings/edit_profile' }]
 
 const Sidebar = () => {
   const { navigate } = useNavigate()
